9e5548abe984a7db9b1f8369351e0e5849e2ac11
Punjasin Punya <punjasin@gmail.com>
2020-10-13 15:57:13 +0700

    make step as components

--- src/components/app.js
+++ src/components/app.js
@@ -1,20 +1,36 @@
-import { h } from 'preact';
-import { Router } from 'preact-router';
+import { h } from "preact";
+import { Router } from "preact-router";
 
 // Code-splitting is automated for `routes` directory
-import Home from '../routes/home';
-import Profile from '../routes/profile';
-import Stepper from '../routes/stepper';
+import Home from "../routes/home";
+import Profile from "../routes/profile";
+import Stepper from "../routes/stepper";
 
 const App = () => (
-	<div id="app">
-		<Router>
-			<Home path="/" />
-			<Profile path="/profile/" user="me" />
-			<Profile path="/profile/:user" />
-			<Stepper path="/stepper" />
-		</Router>
-	</div>
-)
+  <div id="app">
+    <Router>
+      <Home path="/" />
+      <Profile path="/profile/" user="me" />
+      <Profile path="/profile/:user" />
+      <Stepper
+        path="/stepper"
+        contents={[
+          {
+            title: "First",
+            content: "First-content",
+          },
+          {
+            title: "Second",
+            content: "Second-content",
+          },
+          {
+            title: "Last",
+            content: "Last-content",
+          },
+        ]}
+      />
+    </Router>
+  </div>
+);
 
 export default App;
--- src/routes/stepper/index.js
+++ src/routes/stepper/index.js
@@ -5,90 +5,46 @@ import style from "./style.css";
 import { Steps, Button, message, Carousel } from "antd";
 const { Step } = Steps;
 
-const steps = [
-  {
-    title: "First",
-    content: "First-content",
-  },
-  {
-    title: "Second",
-    content: "Second-content",
-  },
-  {
-    title: "Last",
-    content: "Last-content",
-  },
-];
 // Note: `user` comes from the URL, courtesy of our router
 const contentStyle = {
-  height: "160px",
+  height: "100vh",
   color: "#fff",
   lineHeight: "160px",
   textAlign: "center",
   background: "#364d79",
 };
-const Stepper = () => {
+// props is array with component in contents
+const Stepper = ({ contents }) => {
   const [page, setCurrent] = useState(0);
   const stepperContent = useRef(null);
   useEffect(() => {
     try {
-      stepperContent.current.goTo(page);
+      // stepperContent.current.goTo(page); for manipulate page uncomment if want to
     } catch (e) {
       console.log(e);
     }
   }, [page]);
-  const next = () => {
-    const current = page + 1;
-    setCurrent(current);
-  };
 
-  const prev = () => {
-    const current = page - 1;
-    setCurrent(current);
-  };
   const onChange = (a) => {
     setCurrent(a);
   };
   return (
     <PageContainer>
-      <div class={style.profile}>
-        <Steps current={page}>
-          {steps.map((item) => (
-            <Step key={item.title} title={item.title} />
-          ))}
-        </Steps>
-        <div>
-          <Carousel ref={stepperContent} afterChange={onChange}>
-            {steps.map((item) => {
-              return (
-                <div>
-                  <h3 style={contentStyle}>{item.content}</h3>
-                </div>
-              );
-            })}
-          </Carousel>
-        </div>
-        <div className="steps-action">
-          {page < steps.length - 1 && (
-            <Button type="primary" onClick={() => next()}>
-              Next
-            </Button>
-          )}
-          {page === steps.length - 1 && (
-            <Button
-              type="primary"
-              onClick={() => message.success("Processing complete!")}
-            >
-              Done
-            </Button>
-          )}
-          {page > 0 && (
-            <Button style={{ margin: "0 8px" }} onClick={() => prev()}>
-              Previous
-            </Button>
-          )}
-        </div>
-        <p>Stepper Page</p>
+      <div
+        style={{
+          height: "100%",
+        }}
+      >
+        {/*data must be component in contents[index].content*/}
+        <Carousel ref={stepperContent} afterChange={onChange}>
+          {contents.map((item) => {
+            return (
+              <div>
+                <h3 style={contentStyle}>{item.content}</h3>
+              </div>
+            );
+          })}
+        </Carousel>
       </div>
     </PageContainer>
   );
